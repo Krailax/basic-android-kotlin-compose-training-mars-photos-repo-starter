8ff5651eaec327131cdeae01934ec57c915af7b8
Krailax <leif-wither@hotmail.es>
2023-09-29 21:49:43 -0500

    first commit

---
+++ app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -0,0 +1,46 @@
+
+package com.example.marsphotos.ui.screens
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.marsphotos.model.MarsPhoto
+import com.example.marsphotos.network.MarsApi
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+sealed interface MarsUiState {
+    data class Success(val photos: String) : MarsUiState
+    object Error : MarsUiState
+    object Loading : MarsUiState
+}
+
+class MarsViewModel : ViewModel() {
+    var marsUiState: MarsUiState by mutableStateOf(MarsUiState.Loading)
+        private set
+
+
+    init {
+        getMarsPhotos()
+    }
+
+    fun getMarsPhotos() {
+        viewModelScope.launch {
+            marsUiState = MarsUiState.Loading
+            marsUiState = try {
+                val listResult = MarsApi.retrofitService.getPhotos()
+                MarsUiState.Success(
+                    "Éxito:${listResult.size} \n" +
+                            "Fotos de Marte recuperadas \nMarte"
+                )
+            } catch (e: IOException) {
+                MarsUiState.Error
+            } catch (e: HttpException) {
+                MarsUiState.Error
+            }
+        }
+    }
+}
